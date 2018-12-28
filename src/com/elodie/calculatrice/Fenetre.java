bfb96d03baf961b8ac6fde456bd77b9878224cff
elodieOC <elodie.ollivier.pro@gmail.com>
2018-12-28 16:33:12 +0100

    ajout de la javadoc

--- src/com/elodie/calculatrice/Fenetre.java
+++ src/com/elodie/calculatrice/Fenetre.java
@@ -27,29 +27,35 @@ import static java.awt.Color.red;
  * </ol></li>
  * </ul>
  * <p>
- *     On lie ensuite tout ces différents morceaux d'affichage densemble, regroupé dans le corps principal de la calculatrice.
+ *     On lit ensuite tout ces différents morceaux d'affichage densemble, regroupé dans le corps principal de la calculatrice.
  * <p>
- *     On initialise ensuilte l'objet calculatrice depuis lequel seront effectué les calculs
- * <p>
- *     On place un écouteur sur notre objet calculatrice, afin de gérer l'affichage en temps réel.
+ *     On effectue les calculs selon les différents boutons activés de la calculatrice.
  * </p>
+ * @version 1.2
  * @version 1.0
  * @author elojito
  */
 
 class Fenetre extends JFrame {
-
-    //Les Layouts
-    private final GridLayout gridWest = new GridLayout(4, 3, 5, 5); // pour le conteneur numbers
-    private final GridLayout gridEast = new GridLayout(5, 1, 5, 5); // pour le conteneur operators
-
-    // l'écran d'affichage
+    /**
+     *Fenetre possède les attributs:
+     *écran d'affichage
+     *liste d'opérateurs
+     *liste de nombres, virgule ou signe égal
+     *le nom du bouton activé par l'utilisateur
+     * une liste contenant les entrées utilisateurs
+     */
     private final JLabel screened;
     private final String [] ops = {"C", "+", "-", "*", "/"};
+    private final String[] nbr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "="};
     private String clickedOperator;
     //Ajout d'un tableau pour stocker les entrées utilisateur
     private final ArrayList inputs = new ArrayList();
-
+    /**
+     * On forme l'affichage de la calculatrice:
+     * On initialise la fenêtre, le panneau d'affichage, les divers composants de la calculatrice
+     * On met en place l'affichage avec les différents layouts
+     */
     private Fenetre() {
         //On initialise la JFrame
         this.setTitle("Calculatrice");
@@ -62,7 +68,6 @@ class Fenetre extends JFrame {
         JPanel wrapper = new JPanel();
         wrapper.setBackground( Color.white );
 
-        //Init la calculatrice
         Dimension calcSize = new Dimension(270,270);
         // conteneur calculatrice
         JPanel container = new JPanel();
@@ -97,9 +102,11 @@ class Fenetre extends JFrame {
         //Affichage des chiffres
         // conteneur des boutons chiffres
         JPanel numbers = new JPanel();
-        numbers.setLayout(gridWest);
+        //Les Layouts
+        // pour le conteneur numbers
+        GridLayout gridWest = new GridLayout( 4, 3, 5, 5 );
+        numbers.setLayout( gridWest );
         //Les boutons numbers
-        String[] nbr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "="};
         for(String s : nbr){
             Bouton bouton = new Bouton(s);
             bouton.addActionListener(new BoutonListener());
@@ -110,7 +117,9 @@ class Fenetre extends JFrame {
         //Affichage des opérateurs sur la droite
         // conteneur des opérateurs contenu dans operators
         JPanel operatorsInside = new JPanel();
-        operatorsInside.setLayout(gridEast);
+        // pour le conteneur operators
+        GridLayout gridEast = new GridLayout( 5, 1, 5, 5 );
+        operatorsInside.setLayout( gridEast );
         for(String s : ops){
             Bouton bouton = new Bouton(s);
             bouton.addActionListener(new BoutonListener());
@@ -129,29 +138,63 @@ class Fenetre extends JFrame {
         wrapper.add( container );
         this.setContentPane( wrapper );
         this.setVisible(true);
-
     }
 
+    /**
+     *<b>Chaque bouton lorsqu'il est cliqué déclenche une "écoute"</b>
+     *<p>On défini trois grandes actions:
+     *<ul>
+     *     <li>appuyer sur "C" soit Cancel</li>
+     *     <li>appuyer sur "=" soit égal</li>
+     *     <li>appuyer sur toute autre touche</li>
+     *</ul>
+    **/
     class BoutonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
 
+        /**<p>Le détail des différentes actions:</p>
+         * <ul>
+         * <li>Lorsqu'on appuie sur "C":
+         *     <p>
+         *     L'affichage de la calculatrice passe à "0";
+         *     La liste des entrées utilisateurs se vide.
+         *     </p>
+         * </li>
+         * <li>Lorsqu'on appuie sur "=":
+         *     <p>
+         *         La fonction equalOp() se déclenche:
+         *         L'opération est effectuée et l'écran d'affichage renvoi le résultat de celle-ci
+         *         </p>
+         *@see Fenetre#equalOp()
+         * </li>
+         * <li>Lorsqu'on appuie sur toute autre touche de la calcultatrice:
+         *     <p>
+         *         Les entrées utlisateurs sont affichées à l'écran.
+         *         Lorsqu'une opération est faisable suite au déclenchement d'un opérateur, le résultat de celle-ci s'affiche à l'écran,
+         *         la liste des entrées utilisateurs est remplacée par ce résultat.
+         *     </p>
+         * @see Fenetre#clickedOperator
+         * @see Fenetre#plusOp()
+         * @see Fenetre#minusOp()
+         * @see Fenetre#multiplyOp()
+         * @see Fenetre#divideOp()
+         * </li>
+         * </ul>
+         * @param e bouton cliqué
+         */
+        public void actionPerformed(ActionEvent e) {
             Bouton bouton = (Bouton) e.getSource();
             String userInput = bouton.getText();
-            if (userInput == "C") {
-                screened.setText( "0" );
+            if (Objects.equals( userInput, "C" )) {
                 inputs.clear();
+                inputs.add("0");
             }
-            else if(userInput == "="){
+            else if(Objects.equals( userInput, "=" )){
                 equalOp();
             }
             else{
                 inputs.add( userInput );
                 if (Arrays.asList( ops ).contains( userInput )) {
                     clickedOperator = userInput;
-                    /**
-                     * lire le tableau inputs
-                     * trouver l'opérateur et switcher dessus
-                     **/
                     for (Object o : inputs) {
                         if (Arrays.asList( ops ).contains( o )) {
                             switch (o.toString()) {
@@ -181,14 +224,40 @@ class Fenetre extends JFrame {
         }
     }
 
-    private static String myTrimString(String str, String delimiter){
+    /**
+     * Permet de transformer la liste des entrées utilisateurs en chaîne de caractères,
+     * et d'enlever la mise en forme "liste"
+     * @param str chaîne de caractères donnée
+     * @return la chaine de caractères donnée reformatée sans espace et sans caractère spéciaux hormis l'opérateur
+     */
+    private static String myTrimString(String str){
         StringBuilder sb = new StringBuilder();
-        sb.append(str.replaceAll("[\\[\\],]", "").replace( " ", "" )).append(delimiter);
+        sb.append(str.replaceAll("[\\[\\],]", "").replace( " ", "" ));
         str.trim();
         return sb.substring( 0, sb.length());
     }
+
+    /**
+     * Opération suite à l'enclenchement du bouton "=" égal
+     * On récupère la liste des entrées utilisateurs
+     * On cherche quel est l'opérateur utilisé dans l'opération:
+     * <ul>
+     *     <li>Avec la chaîne de caractères récupérée, on utilise la fonction split() en spécifiant qu'on ne cherche ni un chiffre ni un point</li>
+     *     <li>On cherche si cette chaîne de caractères récupérée correspond à une chaîne de caractères contenue dans le tableau des opérateurs</li>
+     *     <li>on assigne ce retour à une chaîne de caractère "lastOperator" qui contient maintenant l'opérateur</li>
+     *     <li>Nous reprenons notre chaîne de caractères des entrées utilisateurs initiale:
+     *     <ul>
+     *         <li>on la divise en deux en utilisant l'opérateur comme délimiteur</li>
+     *         <li>on stocke ces deux parties à deux différentes chaînes de caractères</li>
+     *         <li>on parse ces chaines en "double"</li>
+     *         <li>on effectue l'opération selon l'opérateur défini</li>
+     *         <li>le résultat de celle-ci s'affiche à l'écran, la liste des entrées utilisateurs est remplacée par ce résultat.</li>
+     *     </ul>
+     *     </li>
+     * </ul>
+     */
     private void equalOp() {
-        String lastTrim = myTrimString( inputs.toString(), "" );
+        String lastTrim = myTrimString( inputs.toString());
         String[] findLastOperator = lastTrim.split( "[0-9.]" );
         String lastOperator ="";
         for(String s : findLastOperator){
@@ -198,8 +267,8 @@ class Fenetre extends JFrame {
             }
         }
         String [] nbrACalculer = lastTrim.split("[+]|-|[*]|/");
-        String firstString = myTrimString(nbrACalculer[0], "");
-        String secondString = myTrimString(nbrACalculer[1], "");
+        String firstString = myTrimString(nbrACalculer[0]);
+        String secondString = myTrimString(nbrACalculer[1]);
         double firstNbr = Double.parseDouble( firstString );
         double secondNbr = Double.parseDouble( secondString );
         double res = 0;
@@ -223,10 +292,22 @@ class Fenetre extends JFrame {
             inputs.add(resString);
         }
     }
+
+    /**
+     * Opération suite à l'enclenchement du bouton "+" plus
+     * On récupère la liste des entrées utilisateurs qu'on divise en deux en utilisant un opérateur comme délimiteur
+     *     <ul>
+     *         <li>on stocke ces deux parties à deux différentes chaînes de caractères</li>
+     *         <li>on parse ces chaines en "double"</li>
+     *         <li>on effectue l'addition</li>
+     *         <li>le résultat de celle-ci s'affiche à l'écran, la liste des entrées utilisateurs est remplacée par ce résultat.</li>
+     *         <li>on ajoute à la liste des entrées utilisateurs le dernier opérateur saisi</li>
+     *     </ul>
+     */
     private void plusOp(){
         String [] nbrACalculer = inputs.toString().split("[+]|-|[*]|/");
-        String firstString = myTrimString(nbrACalculer[0], "");
-        String secondString = myTrimString(nbrACalculer[1], "");
+        String firstString = myTrimString(nbrACalculer[0]);
+        String secondString = myTrimString(nbrACalculer[1]);
         if(secondString.length()>=1){
             double firstNbr = Double.parseDouble( firstString );
             double secondNbr = Double.parseDouble( secondString );
@@ -238,10 +319,22 @@ class Fenetre extends JFrame {
 
         }
     }
+
+    /**
+     * Opération suite à l'enclenchement du bouton "-" moins
+     * On récupère la liste des entrées utilisateurs qu'on divise en deux en utilisant un opérateur comme délimiteur
+     *     <ul>
+     *         <li>on stocke ces deux parties à deux différentes chaînes de caractères</li>
+     *         <li>on parse ces chaines en "double"</li>
+     *         <li>on effectue la souscription</li>
+     *         <li>le résultat de celle-ci s'affiche à l'écran, la liste des entrées utilisateurs est remplacée par ce résultat.</li>
+     *         <li>on ajoute à la liste des entrées utilisateurs le dernier opérateur saisi</li>
+     *     </ul>
+     */
     private void minusOp(){
         String [] nbrACalculer = inputs.toString().split("[+]|-|[*]|/");
-        String firstString = myTrimString(nbrACalculer[0], "");
-        String secondString = myTrimString(nbrACalculer[1], "");
+        String firstString = myTrimString(nbrACalculer[0]);
+        String secondString = myTrimString(nbrACalculer[1]);
         if(secondString.length()>=1){
             double firstNbr = Double.parseDouble( firstString );
             double secondNbr = Double.parseDouble( secondString );
@@ -252,10 +345,22 @@ class Fenetre extends JFrame {
             inputs.add( clickedOperator );
         }
     }
+
+    /**
+     * Opération suite à l'enclenchement du bouton "*" multiplier
+     * On récupère la liste des entrées utilisateurs qu'on divise en deux en utilisant un opérateur comme délimiteur
+     *     <ul>
+     *         <li>on stocke ces deux parties à deux différentes chaînes de caractères</li>
+     *         <li>on parse ces chaines en "double"</li>
+     *         <li>on effectue la multiplication</li>
+     *         <li>le résultat de celle-ci s'affiche à l'écran, la liste des entrées utilisateurs est remplacée par ce résultat.</li>
+     *         <li>on ajoute à la liste des entrées utilisateurs le dernier opérateur saisi</li>
+     *     </ul>
+     */
     private void multiplyOp(){
         String [] nbrACalculer = inputs.toString().split("[+]|-|[*]|/");
-        String firstString = myTrimString(nbrACalculer[0], "");
-        String secondString = myTrimString(nbrACalculer[1], "");
+        String firstString = myTrimString(nbrACalculer[0]);
+        String secondString = myTrimString(nbrACalculer[1]);
         if(secondString.length()>=1){
             double firstNbr = Double.parseDouble( firstString );
             double secondNbr = Double.parseDouble( secondString );
@@ -265,10 +370,22 @@ class Fenetre extends JFrame {
             inputs.add(resString);
             inputs.add( clickedOperator );
         }
-    }    private void divideOp(){
+    }
+    /**
+     * Opération suite à l'enclenchement du bouton "/" diviser
+     * On récupère la liste des entrées utilisateurs qu'on divise en deux en utilisant un opérateur comme délimiteur
+     *     <ul>
+     *         <li>on stocke ces deux parties à deux différentes chaînes de caractères</li>
+     *         <li>on parse ces chaines en "double"</li>
+     *         <li>on effectue la division</li>
+     *         <li>le résultat de celle-ci s'affiche à l'écran, la liste des entrées utilisateurs est remplacée par ce résultat.</li>
+     *         <li>on ajoute à la liste des entrées utilisateurs le dernier opérateur saisi</li>
+     *     </ul>
+     */
+    private void divideOp(){
         String [] nbrACalculer = inputs.toString().split("[+]|-|[*]|/");
-        String firstString = myTrimString(nbrACalculer[0], "");
-        String secondString = myTrimString(nbrACalculer[1], "");
+        String firstString = myTrimString(nbrACalculer[0]);
+        String secondString = myTrimString(nbrACalculer[1]);
         if(secondString.length()>=1){
             double firstNbr = Double.parseDouble( firstString );
             double secondNbr = Double.parseDouble( secondString );
